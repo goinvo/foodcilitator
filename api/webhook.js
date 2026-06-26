782416c54c9ed753710df1d5bd52e2139a93efa3
nikiw-red <nikiw@andrew.cmu.edu>
2026-06-26 13:08:25 -0400

    fix: 500 error

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method === "GET") {
     return res.status(200).send("OK");
   }
